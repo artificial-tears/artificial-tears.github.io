9761c23539ac376b2051fa008ae462ac00f20022
Emma Yuan <jinyueyu@usc.edu>
2023-01-13 18:02:56 -0800

    first page style done

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+function firstLoad() {
+    sessionStorage.setItem("loaded", true);
+}
+
+if(sessionStorage.getItem("loaded")) {
+    // display black
+    let bgdiv = document.getElementById("bgimg");
+    bgdiv.innerHTML = `<img src="../assets/FrontPage/black-bg.jpeg" alt="blk">`
+} else {
+    // black with animation
+    let bgdiv = document.getElementById("bgimg");
+    bgdiv.innerHTML = `<img class="fadeInBg" src="../assets/FrontPage/black-bg.jpeg" alt="blk">`
+}
