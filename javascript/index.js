bcbcdc5ab089f11da7b37e2691582b1495c13f28
Emma Yuan <jinyueyu@usc.edu>
2023-01-14 10:28:40 -0800

    moving image v1

--- javascript/index.js
+++ javascript/index.js
@@ -5,9 +5,9 @@ function firstLoad() {
 if(sessionStorage.getItem("loaded")) {
     // display black
     let bgdiv = document.getElementById("bgimg");
-    bgdiv.innerHTML = `<img src="../assets/FrontPage/black-bg.jpeg" alt="blk">`
+    bgdiv.innerHTML = `<img src="../assets/FrontPage/black-bg.jpeg" alt="blk" />`
 } else {
     // black with animation
     let bgdiv = document.getElementById("bgimg");
-    bgdiv.innerHTML = `<img class="fadeInBg" src="../assets/FrontPage/black-bg.jpeg" alt="blk">`
+    bgdiv.innerHTML = `<img class="fadeInBg" src="../assets/FrontPage/black-bg.jpeg" alt="blk" />`
 }
